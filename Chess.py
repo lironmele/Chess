b0f0355539bb04e72437715ea9a52553362671de
Liron Meler <lironmeler@gmail.com>
2020-06-26 15:32:21 +0300

    Add pawn class

--- Chess.py
+++ Chess.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, numpy
 pygame.init()
 
 win = pygame.display.set_mode((500, 500))
@@ -46,6 +46,23 @@ def mouse_selection(win, selection, pieces):
 def draw_selection(win, selection, w, h):
     if selection is not None:
         pygame.draw.rect(win, (0,255,0), ((selection.x // w) * w, (selection.y // h) * h, w, h), 5)
+
+def fill_pieces(board, w, h):
+    pieces = [
+    King(board[4][0],w,h,"B"), Queen(board[3][0],w,h,"W"), Bishop(board[2][0],w,h,"B"),Bishop(board[5][0],w,h,"B"),
+    Knight(board[1][0], w, h, "B"), Knight(board[6][0], w, h, "B"), Rook(board[0][0], w, h, "B"), Rook(board[7][0], w, h, "B")
+    ]
+    
+    for i in range(8):
+        pieces.append(Pawn(board[i][1], w, h, "B"))
+    
+    pieces.extend([King(board[4][7],w,h,"W"), Queen(board[3][7],w,h,"W"), Bishop(board[2][7],w,h,"W"),Bishop(board[5][7],w,h,"W"),
+            Knight(board[1][7], w, h, "W"), Knight(board[6][7], w, h, "W"), Rook(board[0][7], w, h, "W"), Rook(board[7][7], w, h, "W")])
+    
+    for i in range(8):
+        pieces.append(Pawn(board[i][6], w, h, "W"))
+
+    return pieces
 
 class Piece:
     def __init__(self, pos, width, height, piece, team):
@@ -127,16 +144,72 @@ class Knight(Piece):
         if dif_x == 2 and dif_y == 1 or dif_x == 1 and dif_y == 2:
             self.update_pos(pos, pieces)
 
+class Rook(Piece):
+    def __init__(self, pos, width, height, team):
+        super().__init__(pos, width, height, "Rook_" + team, team)
+
+    def move(self, pos, pieces, w, h):
+        if self.hit_team(pos, pieces):
+            return
+        dif_x, dif_y = self.get_dif_pos(pos, w, h)
+        if dif_x == 0 and dif_y != 0 or dif_x != 0 and dif_y == 0:
+            self.update_pos(pos, pieces)
+
+class Pawn(Piece):
+    def __init__(self, pos, width, height, team):
+        super().__init__(pos, width, height, "Pawn_" + team, team)
+        self.first_turn = True
+        
+    def move(self, pos, pieces, w, h):
+        if self.hit_team(pos, pieces):
+            return
+        if pos[1] - self.y > 0 and self.team == "W" or pos[1] - self.y < 0 and self.team == "B":
+            return
+        dif_x, dif_y = self.get_dif_pos(pos, w, h)
+        if dif_x == 0 and dif_y == 1 and not self.is_way_blocked(pos, pieces, h):
+            self.update_pos(pos)
+            self.first_turn = False
+        elif self.first_turn and dif_x == 0 and dif_y == 2 and not self.is_way_blocked(pos, pieces, h):
+            self.update_pos(pos)
+            self.first_turn = False
+        elif dif_x == 1 and dif_y == 1 and self.is_eating(pos, pieces):
+            super().update_pos(pos, pieces)
+            self.first_turn = False
+
+    def is_eating(self, pos, pieces):
+        for piece in pieces:
+            if self is piece or self.team == piece.team:
+                continue
+
+            if pos[0] == piece.x and pos[1] == piece.y:
+                return True
+        return False
+
+    def is_way_blocked(self, pos, pieces, h):
+        for piece in pieces:
+            if self is piece:
+                continue
+      
+            if self.team == "B":
+                for y in numpy.arange(self.y, pos[1] + h, h):  
+                    if pos[0] == piece.x and y == piece.y:
+                            return True
+            elif self.team == "W":
+                for y in numpy.arange(pos[1], self.y + h, h):
+                    if pos[0] == piece.x and y == piece.y:
+                            return True
+        return False
+
+    def update_pos(self, pos):
+        self.x = pos[0]
+        self.y = pos[1]
+        self.rect = (self.x, self.y, self.width, self.height)
+
 def main():
     playing = True
     w, h = get_width_height()
     board = square_pos()
-    pieces = [
-    King(board[4][0],w,h,"B"), Queen(board[3][0],w,h,"B"), Bishop(board[2][0],w,h,"B"),Bishop(board[5][0],w,h,"B"),
-    Knight(board[1][0], w, h, "B"), Knight(board[6][0], w, h, "B"),
-    King(board[4][7],w,h,"W"), Queen(board[3][7],w,h,"W"), Bishop(board[2][7],w,h,"W"),Bishop(board[5][7],w,h,"W"),
-    Knight(board[1][7], w, h, "W"), Knight(board[6][7], w, h, "W")
-    ]
+    pieces = fill_pieces(board, w, h)
     selection = None
     while playing:
         for event in pygame.event.get():
